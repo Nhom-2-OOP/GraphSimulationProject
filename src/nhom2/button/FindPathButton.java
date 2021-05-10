e50aac53cdc51a0dc1857f574a9a6e6f7bf2de7a
doducanh <doducanh2707@gmail.com>
2021-05-10 14:52:19 +0700

    a

---
+++ src/nhom2/button/FindPathButton.java
@@ -0,0 +1,204 @@
+package nhom2.button;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import nhom2.graph.*;
+import nhom2.graphview.*;
+import nhom2.graphview.Edge.EdgeNode;
+import nhom2.graphview.Vertex.VertexNode;
+
+public class FindPathButton<V, E> extends Button {
+	private final int LENGTH_LIST_VERTEX = 5;
+	private final int MAX_STEP = 20;
+	private Stage stage;
+	protected Scene View;
+	private Vertex<V> currentVertex;
+	private Vertex<V> endVertex;
+	private int countStep = 0;
+	TextField textPath = new TextField("");
+
+	public void a(GridPane grid, GraphPanel<V, E> graphView, Vertex<V> v) {
+		// lay ra danh sach dinh ke, canh ke
+		Map<Vertex<V>, Edge<E, V>> unknown = graphView.theGraph.adjList.get(v);
+		Set<Vertex<V>> adjVertex = unknown.keySet();
+
+		ChoiceBox<String> listNextVertex = new ChoiceBox();
+		int cntV = 0; // so dinh ke hien thi ra
+		// ObservableList<String> nextList = FXCollections.observableArrayList();
+
+		for (Vertex<V> iterator : adjVertex) {
+			// nextList.add(iterator.element().toString());
+			String mi = new String(iterator.element().toString());
+			listNextVertex.getItems().add(mi);
+			cntV++;
+			if (cntV >= LENGTH_LIST_VERTEX)
+				break;
+		}
+
+		listNextVertex.setMinWidth(50);
+		grid.add(listNextVertex, 3, 1);
+
+		// sự kiện click chon dinh
+
+		listNextVertex.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				String input = listNextVertex.getValue();
+				Vertex<V> inputVertex = graphView.theGraph.vertices.get(input);
+				VertexNode<V> inputVertexNode = graphView.vertexNodes.get(inputVertex);
+				Edge<E, V> inputEdge = graphView.theGraph.adjList.get(currentVertex).get(inputVertex);
+				EdgeNode<E, V> inputEdgeNode = graphView.edgeNodes.get(inputEdge);
+
+				// thay đổi màu đỉnh và cạnh
+				inputVertexNode.setStyle("-fx-fill: yellow");
+				inputEdgeNode.setStyle("-fx-stroke: blue");
+				inputEdgeNode.getAttachedArrow().setStyle("-fx-stroke: blue");
+				textPath.appendText(input + " ->");
+				currentVertex = inputVertex;
+			}
+		});
+
+	}
+
+	public FindPathButton(Stage s, GraphPanel<V, E> graphView) {
+		GridPane grid = new GridPane();
+		TextField tfEndVertex = new TextField();
+		TextField tfStartVertex = new TextField();
+		Button btFind = new Button("Find");
+		Button btn = new Button("OK");
+		Button reset = new Button("Reset");
+		btn.setVisible(false);
+		reset.setVisible(false);
+
+		textPath.setEditable(false);
+		tfStartVertex.setPromptText("Dinh bat dau");
+		tfStartVertex.setPrefWidth(85);
+		tfStartVertex.setMaxWidth(85);
+		tfEndVertex.setPromptText("Dinh ket thuc");
+		tfEndVertex.setPrefWidth(85);
+		tfEndVertex.setMaxWidth(85);
+
+		grid.add(new Label("StartVertex"), 0, 0);
+		grid.add(tfStartVertex, 1, 0);
+		grid.add(new Label("EndVertex"), 0, 1);
+		grid.add(tfEndVertex, 1, 1);
+		grid.add(btFind, 1, 2);
+		grid.add(new Label("Path"), 0,3);
+		grid.add(textPath, 1, 3);
+		grid.add(btn, 3, 2);
+		grid.add(reset, 3, 3);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(10, 10, 100, 10));
+		grid.setMinSize(500, 200);
+
+		try {
+			Parent root = grid;
+			this.View = new Scene(root);
+			this.stage = s;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		this.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				stage.setScene(View);
+				stage.show();
+			}
+		});
+
+		btFind.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				tfStartVertex.commitValue();
+				String dataStart = tfStartVertex.getText();
+				tfEndVertex.commitValue();
+				String dataEnd = tfEndVertex.getText();
+
+				// thuat toan
+				Vertex<V> startVertex = graphView.theGraph.vertices.get(dataStart);
+				endVertex = graphView.theGraph.vertices.get(dataEnd);
+				// chuyển màu riêng 2 đỉnh start và end
+				VertexNode<V> startVertexNode = graphView.vertexNodes.get(startVertex);
+				VertexNode<V> endVertexNode = graphView.vertexNodes.get(endVertex);
+				startVertexNode.setStyle("-fx-fill: red");
+				endVertexNode.setStyle("-fx-fill: red");
+				
+				if (startVertex == null || endVertex == null) {
+					Alert alert = new Alert(AlertType.CONFIRMATION);
+					alert.setHeaderText("Đỉnh không tồn tại trong đồ thị");
+					alert.show();
+					return;
+				}
+				
+				textPath.appendText(dataStart + " ->");
+				currentVertex = startVertex;
+				grid.add(new Label("Choose next vertex then click OK"), 3, 0);
+				btn.setVisible(true);
+				reset.setVisible(true);
+				a(grid, graphView, currentVertex);
+			}
+		});
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				countStep++;
+				if (countStep > MAX_STEP) {
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setContentText("The path has reached its limit step!");
+					alert.showAndWait();
+					return;
+				}
+				if (currentVertex.equals(endVertex)) {
+					textPath.appendText("Success");
+					graphView.vertexNodes.get(currentVertex).setStyle("-fx-fill: red");
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setContentText("Done!");
+					alert.showAndWait();
+					stage.close();
+					return;
+				}
+				// TODO Auto-generated method stub
+				a(grid, graphView, currentVertex);
+			}
+		});
+		reset.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				for (VertexNode<V> tmp : graphView.vertexNodes.values())
+					tmp.setStyle("-fx-fill: #96d1cd");
+				for (EdgeNode<E, V> tmp : graphView.edgeNodes.values()) {
+					tmp.setStyle("-fx-stroke: #45597e");
+					tmp.getAttachedArrow().setStyle(" -fx-stroke: #45597e");
+				}
+				countStep =0;
+				textPath.setText("");
+			}
+
+		});
+	}
+}
--- src/nhom2/window/Main.java
+++ src/nhom2/window/Main.java
@@ -8,6 +8,8 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.layout.*;
@@ -16,6 +18,7 @@ import javafx.stage.StageStyle;
 import nhom2.button.AutoPlacementButton;
 import nhom2.button.CaptureGraphPanel;
 import nhom2.button.CircularPlacementButton;
+import nhom2.button.FindPathButton;
 import nhom2.button.InputButton;
 import nhom2.graph.*;
 import nhom2.graphview.*;
@@ -29,38 +32,50 @@ public class Main extends Application {
     	
     	// Tao scene bieu dien do thi
     	graphView = new GraphPanel<>(g);
-    	SubScene subSceneOne = new SubScene(graphView,500,500);
+    	SubScene subSceneGraphPanel = new SubScene(graphView,600,600);
     	graphView.init();
         //graphView.start_automatic_layout();
         
         // Tao nut sap dinh theo vong tron
-    	CircularPlacementButton ButtonOne = new CircularPlacementButton(graphView);
-    	ButtonOne.setText("Sắp đỉnh theo vòng tròn");
-    	SubScene subSceneTwo = new SubScene(ButtonOne,150,30);
+    	CircularPlacementButton btnCircularPla = new CircularPlacementButton(graphView);
+    	btnCircularPla.setText("Sắp đỉnh theo vòng tròn");
+    	SubScene subSceneCircularPla = new SubScene(btnCircularPla,150,30);
     	
     	// Tao nut sap dinh tu dong
-    	AutoPlacementButton ButtonTwo = new AutoPlacementButton(graphView);
-    	ButtonTwo.setText("Sắp đỉnh tự động");
-    	SubScene subSceneThree = new SubScene(ButtonTwo,150,30);
+    	AutoPlacementButton btnAutoPla = new AutoPlacementButton(graphView);
+    	btnAutoPla.setText("Sắp đỉnh tự động");
+    	SubScene subSceneAutoPla = new SubScene(btnAutoPla,150,30);
     	
     	// Tao nut luu anh do thi
-    	CaptureGraphPanel ButtonThree = new CaptureGraphPanel(subSceneOne, stage);
-    	ButtonThree.setText("Lưu ảnh đồ thị");
-    	SubScene subSceneFour = new SubScene(ButtonThree,150,30);
+    	CaptureGraphPanel btnCaptureGP = new CaptureGraphPanel(subSceneGraphPanel, stage);
+    	btnCaptureGP.setText("Lưu ảnh đồ thị");
+    	SubScene subSceneCaptureGP = new SubScene(btnCaptureGP,150,30);
     	
     	// Tao nut input do thi
-    	InputButton ButtonFour = new InputButton(stage);
-    	ButtonFour.setText("Input đồ thị");
-    	SubScene subSceneFive = new SubScene(ButtonFour,150,30);
-    	
+    	InputButton btnInput = new InputButton(stage);
+    	btnInput.setText("Input đồ thị");
+    	SubScene subSceneInput = new SubScene(btnInput,150,30);
+
+    	// Tao nut tim duong di
+    	FindPathButton<String, String> btnFindPath = new FindPathButton(stage, graphView);
+    	btnFindPath.setText("Tìm đường đi");
+    	SubScene subSceneFindPath = new SubScene(btnFindPath,150,30);
+
     	// Tao layout VBox
-    	VBox root = new VBox(10);
-    	root.getChildren().addAll(subSceneOne, subSceneTwo, subSceneThree, subSceneFour, subSceneFive);
+    	VBox buttonArea = new VBox(10);
+    	buttonArea.getChildren().addAll(subSceneCircularPla, subSceneAutoPla, subSceneCaptureGP, subSceneInput, subSceneFindPath);
+    	buttonArea.setAlignment(Pos.CENTER);
+
+    	GridPane root = new GridPane();
+    	root.add(buttonArea, 0, 0);
+    	root.add(subSceneGraphPanel, 1, 0);
+    	root.setHgap(20);
+    	root.setPadding(new Insets(10, 10, 100, 10));
     	
-    	Scene scene = new Scene(root, 521, 650);
+    	Scene scene = new Scene(root, 650, 650);
         stage = new Stage();
         stage.setTitle("Graph Visualization");
-        stage.setMinHeight(500);
+        stage.setMinHeight(800);
         stage.setMinWidth(800);
         stage.setScene(scene);
         stage.show();  
